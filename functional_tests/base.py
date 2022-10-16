8066ac7ba85cb788ed41ef7cddf81b6fce60f6ed
JavierHerreroPorras <javierherreroporras@gmail.com>
2022-10-16 18:39:37 +0200

    Remove screenshots

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -14,7 +14,7 @@ from functional_tests.server_tools import reset_database, create_session_on_serv
 MAX_WAIT = 10
 
 SCREEN_DUMP_LOCATION = os.path.join(
-	os.path.dirname(os.path.abspath(__file__)), 'screeendumps'
+	os.path.dirname(os.path.abspath(__file__)), 'screendumps'
 )
 
 
