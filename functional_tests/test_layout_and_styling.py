7610b9c620a65050a3a8f4f2f472facdeab4ab9b
JavierHerreroPorras <javierherreroporras@gmail.com>
2022-10-16 12:19:34 +0200

    Make style test more flexible

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -15,7 +15,7 @@ class LayoutAndStylingTest(FunctionalTest):
 		self.assertAlmostEqual(
 			inputbox.location['x'] + inputbox.size['width'] / 2,
 			512,
-			delta=10
+			delta=100
 		)
 
 		# She starts a new list and sees the input is nicely centered there too
